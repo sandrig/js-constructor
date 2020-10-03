898829706bb767ed2f7076400cd383fffedb0c49
sandrig <sandrig.dev@gmail.com>
2020-10-03 15:21:03 +0300

    refactor: image styles

--- src/model.js
+++ src/model.js
@@ -1,20 +1,64 @@
 import image from './assets/image.png'
 
+const text = `
+Крутые видео и уроки по JavaScript. Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам, такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!
+`
+
 export const model = [
-  {type: 'title', value: 'Конструктор сайтов на чистом JavaScript', options: {
+  {
+    type: 'title',
+    value: 'Конструктор сайтов на чистом JavaScript',
+    options: {
       tag: 'h2',
       styles: {
-        background: 'background: linear-gradient(to right, #ff0099, #493240)',
+        background: 'linear-gradient(to right, #ff0099, #493240)',
         color: '#fff',
         padding: '1.5rem',
         'text-align': 'center'
       }
-  }},
-  {type: 'text', value: 'Here we go with some text'},
-  {type: 'columns', value: [
-      '11111111',
-      '22222222',
-      '33333333'
-    ]},
-  {type: 'image', value: image}
+    }
+  },
+  {
+    type: 'image',
+    value: image,
+    options: {
+      styles: {
+        padding: '2rem 0',
+        display: 'flex',
+        'justify-content': 'center'
+      },
+      imageStyles: {
+        width: '500px',
+        height: 'auto'
+      },
+      alt: 'Это картинка'
+    }
+  },
+  {
+    type: 'columns',
+    value: [
+      'Приложение на чистом JavaScript, без использования библиотек',
+      'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
+      'JavaScript - это просто, интересно. Научись создавать любые UI своими руками'
+    ],
+    options: {
+      styles: {
+        background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+        padding: '2rem',
+        color: '#fff',
+        'font-weight': 'bold'
+      }
+    }
+  },
+  {
+    type: 'text',
+    value: text,
+    options: {
+      styles: {
+        background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+        padding: '1rem',
+        'font-weight': 'bold'
+      }
+    }
+  }
 ]
--- src/templates.js
+++ src/templates.js
@@ -6,16 +6,17 @@ function title(block) {
 }
 
 function text(block) {
-  return row(col(`<p>${block.value}</p>`))
+  return row(col(`<p>${block.value}</p>`), css(block.options.styles))
 }
 
 function columns(block) {
   const html = block.value.map(col).join('')
-  return row(html)
+  return row(html, css(block.options.styles))
 }
 
 function image(block) {
-  return row(`<img src="${block.value}">`)
+  const { imageStyles, styles, alt = '' } = block.options
+  return row(`<img src="${block.value}" alt="${alt}" style="${css(imageStyles)}" />`, css(styles))
 }
 
 export const templates = { title, text, columns, image }
