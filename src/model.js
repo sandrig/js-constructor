cad43664b7f0af5565f19f68474e6056a8304785
sandrig <sandrig.dev@gmail.com>
2020-10-03 15:01:02 +0300

    refactor: styles

--- src/model.js
+++ src/model.js
@@ -3,8 +3,13 @@ import image from './assets/image.png'
 export const model = [
   {type: 'title', value: 'Конструктор сайтов на чистом JavaScript', options: {
       tag: 'h2',
-      styles: `background: linear-gradient(to right, #ff0099, #493240); color: #fff; text-align: center; padding: 1.5rem;`
-    }},
+      styles: {
+        background: 'background: linear-gradient(to right, #ff0099, #493240)',
+        color: '#fff',
+        padding: '1.5rem',
+        'text-align': 'center'
+      }
+  }},
   {type: 'text', value: 'Here we go with some text'},
   {type: 'columns', value: [
       '11111111',
