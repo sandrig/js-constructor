fee22a02bc2e2ea45b420f4932b8427ba4ea1e95
sandrig <sandrig.dev@gmail.com>
2020-10-03 14:52:57 +0300

    feat: add styles

--- src/model.js
+++ src/model.js
@@ -1,7 +1,10 @@
 import image from './assets/image.png'
 
 export const model = [
-  {type: 'title', value: 'Hello world from JS'},
+  {type: 'title', value: 'Конструктор сайтов на чистом JavaScript', options: {
+      tag: 'h2',
+      styles: `background: linear-gradient(to right, #ff0099, #493240); color: #fff; text-align: center; padding: 1.5rem;`
+    }},
   {type: 'text', value: 'Here we go with some text'},
   {type: 'columns', value: [
       '11111111',
