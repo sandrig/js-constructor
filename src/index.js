75a47cfbb2ef4e33ede651087a884bf1272b990b
sandrig <sandrig.dev@gmail.com>
2020-10-03 17:02:17 +0300

    feat: add generate blocks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import { model } from './model'
 import { Site } from './classes/site'
+import { Sidebar } from './classes/sidebar'
 import './styles/main.css'
 
 const site = new Site('#site')
 
 site.render(model)
+
+const updateCallback = newBlock => {
+  model.push(newBlock)
+  site.render(model)
+}
+
+new Sidebar('#panel', updateCallback)
