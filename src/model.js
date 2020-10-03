7623642f53f881587595e540e91856aa392de014
sandrig <sandrig.dev@gmail.com>
2020-10-03 16:04:35 +0300

    refactor: add block class

--- src/model.js
+++ src/model.js
@@ -1,3 +1,4 @@
+import { Block } from './classes/blocks'
 import image from './assets/image.png'
 
 const text = `
@@ -5,60 +6,44 @@ const text = `
 `
 
 export const model = [
-  {
-    type: 'title',
-    value: 'Конструктор сайтов на чистом JavaScript',
-    options: {
-      tag: 'h2',
-      styles: {
-        background: 'linear-gradient(to right, #ff0099, #493240)',
-        color: '#fff',
-        padding: '1.5rem',
-        'text-align': 'center'
-      }
+  new Block('title', 'Конструктор сайтов на чистом JavaScript', {
+    tag: 'h2',
+    styles: {
+      background: 'linear-gradient(to right, #ff0099, #493240)',
+      color: '#fff',
+      padding: '1.5rem',
+      'text-align': 'center'
     }
-  },
-  {
-    type: 'image',
-    value: image,
-    options: {
-      styles: {
-        padding: '2rem 0',
-        display: 'flex',
-        'justify-content': 'center'
-      },
-      imageStyles: {
-        width: '500px',
-        height: 'auto'
-      },
-      alt: 'Это картинка'
-    }
-  },
-  {
-    type: 'columns',
-    value: [
-      'Приложение на чистом JavaScript, без использования библиотек',
-      'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
-      'JavaScript - это просто, интересно. Научись создавать любые UI своими руками'
-    ],
-    options: {
+  }),
+  new Block('image', image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto'
+    },
+    alt: 'Это картинка'
+  }),
+  new Block('columns', [
+    'Приложение на чистом JavaScript, без использования библиотек',
+    'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
+    'JavaScript - это просто, интересно. Научись создавать любые UI своими руками'
+  ], {
       styles: {
         background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
         padding: '2rem',
         color: '#fff',
         'font-weight': 'bold'
       }
+  }),
+  new Block('text', text, {
+    styles: {
+      background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+      padding: '1rem',
+      'font-weight': 'bold'
     }
-  },
-  {
-    type: 'text',
-    value: text,
-    options: {
-      styles: {
-        background: 'linear-gradient(to left, #f2994a, #f2c94c)',
-        padding: '1rem',
-        'font-weight': 'bold'
-      }
-    }
-  }
+  })
 ]
