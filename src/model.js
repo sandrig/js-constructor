3cf917f583f186d6ea0c9cf77541ccf6e757e53b
sandrig <sandrig.dev@gmail.com>
2020-10-03 13:22:23 +0300

    refactor: project structure

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+  {type: 'title', value: 'Hello world from JS'},
+  {type: 'text', value: 'Here we go with some text'},
+  {type: 'columns', value: [
+      '11111111',
+      '22222222',
+      '33333333'
+    ]},
+  {type: 'image', value: './assets/image.png'}
+]
