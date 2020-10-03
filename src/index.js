f58766a81576d33f153957f419f7cc301fe767ee
sandrig <sandrig.dev@gmail.com>
2020-10-03 17:04:51 +0300

    refactor: app component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import { model } from './model'
-import { Site } from './classes/site'
-import { Sidebar } from './classes/sidebar'
+import { App } from './classes/app'
 import './styles/main.css'
 
-const site = new Site('#site')
-
-site.render(model)
-
-const updateCallback = newBlock => {
-  model.push(newBlock)
-  site.render(model)
-}
-
-new Sidebar('#panel', updateCallback)
+new App(model).init()
